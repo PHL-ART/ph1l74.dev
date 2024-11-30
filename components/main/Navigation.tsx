b44d2c52842959ff68a60d1fbe211cdc3eb25184
Filat Astakhov <astakhovfilat@gmail.com>
2024-11-30 09:25:45 +0300

    Animation appended

--- components/main/Navigation.tsx
+++ components/main/Navigation.tsx
@@ -10,7 +10,7 @@ const MainNavigationLink: FC<MainNavigationLinkProps> = ({
   return (
     <Link
       href={href}
-      className="flex flex-col p-2 rounded-lg grow shrink basis-0 w-0 hover:bg-white/15 gap-4"
+      className="flex flex-col p-2 rounded-lg grow shrink scale-95 transition-all duration-200 ease-in-out basis-0 w-0 hover:bg-white/15 gap-4 hover:animate-nav-card-on hover:scale-100  "
     >
       <div className="flex flex-row justify-between">
         <div className="text-2xl font-bold">{title}</div>
